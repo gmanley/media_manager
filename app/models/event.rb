b52baad4c57af8d6b554d64814c628a188c95f71
Gray Manley <gray.manley@gmail.com>
2012-05-06 16:51:02 -0500

    Remove no longer used models.

--- app/models/event.rb
+++
@@ -1,8 +0,0 @@
-class Event
-  include Mongoid::Document
-  include Mongoid::Timestamps
-
-  has_many :segments
-
-  field :name, type: String
-end
--- app/models/performance.rb
+++
@@ -1,6 +0,0 @@
-class Performance < Segment
-  has_many :artists, as: :attributable
-  belongs_to :event
-
-  field :name, type: String
-end
