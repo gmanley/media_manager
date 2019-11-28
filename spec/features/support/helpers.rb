138e5ef783ff1ab07724142b922b15f1ed53ea45
Gray Manley <gray.manley@gmail.com>
2019-11-27 19:30:56 -0600

    Remove bad tests

--- spec/features/support/helpers.rb
+++ spec/features/support/helpers.rb
@@ -1,13 +1,3 @@
 module HelperMethods
 
-  def sign_in(user)
-    visit homepage
-
-    within 'form#login' do
-      fill_in 'user_email', with: user.email
-      fill_in 'user_password', with: user.password
-
-      click_button 'Sign In'
-    end
-  end
 end
