d5889808ff9413dbb814944feb3c31f6679a789f
Grayson Manley <gmanley@eligoenergy.com>
2019-12-02 20:18:04 -0600

    Update dependencies

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,2 @@
 Sidekiq.configure_server do |config|
-  config.server_middleware do |chain|
-    chain.remove Sidekiq::Middleware::Server::RetryJobs
-    chain.remove Sidekiq::Middleware::Server::ActiveRecord
-  end
 end
