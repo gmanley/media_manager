53f50f9fa78f5efc50e27e8d069a533b0d97b514
Gray Manley <gray.manley@gmail.com>
2019-12-27 20:43:30 -0600

    Fix issue with square pixel change.

--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -17,12 +17,15 @@ class Snapshot < ApplicationRecord
     Dir.mktmpdir do |dir|
       snapshot_path = File.join(dir, "#{File.basename(video.file_path)}_snapshot#{video_time}.jpg")
 
-      scale_args = "-vf scale=#{video.calculated_width}:#{video.height} \\"
-      if system("ffmpeg -ss #{video_time} \
-                  -i #{video.file_path.shellescape} \
-                  -vframes 1 -an -f rawvideo -vcodec mjpeg \
-                  #{scale_args if video.non_square_pixel?}
-                  #{snapshot_path.shellescape} >/dev/null 2>&1")
+      args = [
+        "ffmpeg -ss #{video_time}",
+        "-i #{video.file_path.shellescape}",
+        "-vframes 1 -an -f rawvideo -vcodec mjpeg"
+      ].join
+      args << "-vf scale=#{video.calculated_width}:#{video.height}" if video.non_square_pixel?
+      args << "#{snapshot_path.shellescape} >/dev/null 2>&1"
+
+      if system(args.join(' '))
         self.image = File.open(snapshot_path)
       else
         false
