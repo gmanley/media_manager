328f9a1835eb1a76a33bb5e65d336b32e1e6685e
Grayson Manley <gmanley@eligoenergy.com>
2020-01-22 20:09:47 -0600

    Pass full mail object to verify mega  service

--- app/services/verify_mega_account.rb
+++ app/services/verify_mega_account.rb
@@ -1,13 +1,13 @@
 class VerifyMegaAccount
-  def initialize(email_body:)
-    @email_body = email_body
+  def initialize(mail:)
+    @mail = mail
   end
 
   def perform
-    doc = Nokogiri::HTML(@email_body)
+    doc = Nokogiri::HTML(@mail.html_part.body.to_s)
     verify_link = doc.at('//a[text()[contains(., "Verify my email")]]')
     if verify_link
-      account_email = mail.to.first
+      account_email = @mail.to.first
       account = HostProviderAccount.find_by(username: account_email)
 
       command = account.info['verify_command'].gsub('@LINK@', verify_link[:href])
