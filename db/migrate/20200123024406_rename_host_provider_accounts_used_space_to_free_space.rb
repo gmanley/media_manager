47bcfc7fc2a6e42d6e8aeb087934f98448f4430e
Gray Manley <gray.manley@gmail.com>
2020-01-22 20:56:23 -0600

    Make freespace a bigint

--- db/migrate/20200123024406_rename_host_provider_accounts_used_space_to_free_space.rb
+++ db/migrate/20200123024406_rename_host_provider_accounts_used_space_to_free_space.rb
@@ -1,5 +1,11 @@
 class RenameHostProviderAccountsUsedSpaceToFreeSpace < ActiveRecord::Migration[6.0]
-  def change
+  def up
     rename_column :host_provider_accounts, :used_space, :free_space
+    change_column :host_provider_accounts, :free_space, :bigint
+  end
+
+  def down
+    rename_column :host_provider_accounts, :free_space, :used_space
+    change_column :host_provider_accounts, :used_space, :integer
   end
 end
