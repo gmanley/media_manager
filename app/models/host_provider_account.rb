3b6e416f1b2e455e103f5a48838d2d5d96963137
Gray Manley <gray.manley@gmail.com>
2020-01-23 20:50:48 -0600

    Tweaks

--- app/models/host_provider_account.rb
+++ app/models/host_provider_account.rb
@@ -2,6 +2,19 @@ class HostProviderAccount < ApplicationRecord
   belongs_to :host_provider
   has_many :uploads
 
+  validates :username, presence: true
+  validates :password, presence: true
+  validates :host_provider, presence: true
+  validates :name, presence: true, if: -> { host_provider.name == 'mega' }
+
+  def self.online
+    where(online: true)
+  end
+
+  def self.for_provider(provider)
+    joins(:host_provider).where(host_providers: { name: provider })
+  end
+
   def client
     @client ||= HostProviders[host_provider.name].new(
       username: username,
@@ -9,9 +22,13 @@ class HostProviderAccount < ApplicationRecord
     )
   end
 
+  def free_storage
+    total_storage - used_storage
+  end
+
   def check_and_update_free_space
     if client.check_usage?
-      update(free_space: client.status[:free_space])
+      update(used_storage: client.status[:used_storage])
     end
   end
 end
