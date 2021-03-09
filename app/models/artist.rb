2e160a72def8ae111a47f5e126b2557459d1abff
Gray Manley <gray.manley@gmail.com>
2021-03-08 22:16:35 -0600

    Remove unused files

--- app/models/artist.rb
+++
@@ -1,7 +0,0 @@
-# class Artist < ApplicationRecord
-#   has_many :members, class_name: self.name, inverse_of: :group, foreign_key: :group_id, autosave: true
-#   belongs_to :group, class_name: self.name, inverse_of: :members, index: true
-#   belongs_to :attributable, polymorphic: true
-
-#   field :name, type: String
-# end
--- app/models/event.rb
+++
@@ -1,6 +0,0 @@
-# class Event < ApplicationRecord
-#   has_many :performances
-
-#   field :name, type: String
-#   field :possible_names, type: Array
-# end
